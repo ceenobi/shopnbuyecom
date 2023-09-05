b97f7b8a8490578801f721de4bd96725919f2e24
ceenobi <cobimbachu@gmail.com>
2023-09-05 14:28:15 +0100

    fix bag page

--- client/src/pages/ViewBag.jsx
+++ client/src/pages/ViewBag.jsx
@@ -44,88 +44,93 @@ export default function ViewBag() {
         </Row>
         <hr style={{ border: '1px solid black' }} />
       </div>
-
-      {bagItems.map((item) => (
-        <div key={item._id}>
-          <Row className='align-items-center'>
-            <Col xs={8} md={5} className='mb-4'>
-              <div className='d-flex gap-2'>
-                <Link to={`/collections/${item.category}/${item.slug}`}>
-                  <Image
-                    src={item.images[0]}
-                    alt={item.title}
-                    key={item.id}
-                    style={{ width: '100px', height: 'auto' }}
-                  />
-                </Link>
-                <div>
-                  <p className='fs-6 fw-bold mb-0'>{item.title}</p>
-                  <span className='fs-6'>{item.brand}</span>
-                </div>
-              </div>
-            </Col>
-            <Col xs={4} md={2} className='mb-4'>
-              <p className='fs-5'>{formatCurrency(item.price)}</p>
-            </Col>
-            <Col xs={8} md={3} className='mb-4'>
-              <div className='d-flex gap-1 align-items-center qtyBox'>
-                <div
-                  className='d-flex gap-2 justify-content-between align-items-center border border-black bg-white rounded-1 p-2'
-                  style={{ width: '100px' }}
+      {bagItems.length > 0 ? (
+        <>
+          {bagItems.map((item) => (
+            <div key={item._id}>
+              <Row className='align-items-center'>
+                <Col xs={8} md={5} className='mb-4'>
+                  <div className='d-flex gap-2 align-items-center'>
+                    <Link to={`/collections/${item.category}/${item.slug}`}>
+                      <Image
+                        src={item.images[0]}
+                        alt={item.title}
+                        key={item.id}
+                        style={{ width: '120px', height: 'auto' }}
+                      />
+                    </Link>
+                    <div>
+                      <p className='fs-6 fw-bold mb-0'>{item.title}</p>
+                      <span className='fs-6'>{item.brand}</span>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={4} md={2} className='mb-4'>
+                  <p className='fs-5'>{formatCurrency(item.price)}</p>
+                </Col>
+                <Col xs={8} md={3} className='mb-4'>
+                  <div className='d-flex gap-1 align-items-center qtyBox'>
+                    <div
+                      className='d-flex gap-2 justify-content-between align-items-center border border-black bg-white p-2'
+                      style={{ width: '100px' }}
+                    >
+                      <Minus
+                        style={{ cursor: 'pointer' }}
+                        size='16px'
+                        onClick={() => decreaseBagQuantity(item)}
+                      />
+                      <span className='fs-5 fw-medium'>{item.quantity}</span>
+                      <Plus
+                        style={{ cursor: 'pointer' }}
+                        size='16px'
+                        onClick={() => increaseBagQuantity(item)}
+                      />
+                    </div>
+                    <Trash2
+                      style={{ cursor: 'pointer' }}
+                      size='16px'
+                      onClick={() => removeFromBag(item._id)}
+                      className='hideTrash'
+                    />
+                  </div>
+                </Col>
+                <Col xs={4} md={2} className='mb-2'>
+                  <p className='fs-5'>
+                    {formatCurrency(item.quantity * item.price)}
+                  </p>
+                </Col>
+              </Row>
+              <hr style={{ border: '1px solid black' }} />
+            </div>
+          ))}
+          <div className='d-flex justify-content-lg-end mt-4'>
+            <div>
+              <h1>
+                <span className='fs-4'>Subtotal: </span>{' '}
+                <span className='fw-bold'>{formatCurrency(priceTotal)}</span>
+              </h1>
+              <span className='fs-6 mb-4'>
+                Taxes and shipping calculated at checkout
+              </span>
+              <div className='mt-3'>
+                <Button
+                  variant='dark'
+                  className='rounded-0 fw-bold w-100'
+                  onClick={() => {
+                    currentUser
+                      ? navigate('/checkout')
+                      : toast.error('Please Sign in to continue')
+                  }}
                 >
-                  <Minus
-                    style={{ cursor: 'pointer' }}
-                    size='16px'
-                    onClick={() => decreaseBagQuantity(item)}
-                  />
-                  <span className='fs-5 fw-medium'>{item.quantity}</span>
-                  <Plus
-                    style={{ cursor: 'pointer' }}
-                    size='16px'
-                    onClick={() => increaseBagQuantity(item)}
-                  />
-                </div>
-                <Trash2
-                  style={{ cursor: 'pointer' }}
-                  size='16px'
-                  onClick={() => removeFromBag(item._id)}
-                  className='hideTrash'
-                />
+                  Checkout
+                </Button>
               </div>
-            </Col>
-            <Col xs={4} md={2} className='mb-2'>
-              <p className='fs-5'>
-                {formatCurrency(item.quantity * item.price)}
-              </p>
-            </Col>
-          </Row>
-          <hr style={{ border: '1px solid black' }} />
-        </div>
-      ))}
-      <div className='d-flex justify-content-lg-end mt-4'>
-        <div>
-          <h1>
-            <span className='fs-4'>Subtotal: </span>{' '}
-            <span className='fw-bold'>{formatCurrency(priceTotal)}</span>
-          </h1>
-          <span className='fs-6 mb-4'>
-            Taxes and shipping calculated at checkout
-          </span>
-          <div className='mt-3'>
-            <Button
-              variant='dark'
-              className='rounded-0 fw-bold w-100'
-              onClick={() => {
-                currentUser
-                  ? navigate('/checkout')
-                  : toast.error('Please Sign in to continue')
-              }}
-            >
-              Checkout
-            </Button>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <h1 className='text-center fs-4 mt-4'>Your bag is empty 😭 </h1>
+      )}
     </PageLayout>
   )
 }
